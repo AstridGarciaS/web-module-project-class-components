d1c6a07dc3b85109b6e09ced2219001cbcd483f2
AstridGarciaS <119351821+AstridGarciaS@users.noreply.github.com>
2023-07-02 14:24:01 -0400

    update

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import TodoList from './TodoList';
+import Form from './Form';
+
 
 export default class App extends React.Component {
   constructor(){
@@ -6,38 +9,62 @@ export default class App extends React.Component {
     this.state = {
       todos: [
         {
-          name: 'Organize Garage',
+          task: 'Organize Garage',
           id: 1528817077286, 
           completed: true
         },
         {
-          name: 'Bake Cookies',
+          task: 'Bake Cookies',
           id: 1528817084358,
+          completed: false
+        },
+        {
+          task: 'eat Cookies',
+          id: 1528817384358,
           completed: false
         }
       ]
     }
     
   }
+
+  handleAdd = (task) => {
+
+    const newTodo = {
+      task: task,
+      id: Date.now(),
+      completed: false
+    };
+
+
+
+    this.setState({
+      ...this.state,
+      todos: [...this.state.todos, newTodo]
+
+    })
+  }
+
+  handleClear = () => {
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.filter(todo => {
+        return (todo.completed === false);
+      })
+    });
+  }
+
+
+
   render() {
     const { todos } = this.state;
     return (
       <div>
         <h2>Welcome to my Todo App!</h2>
-        <ul>
-          {
-            todos.map(todo=> {
-              return (<li key={todo.id}>{todo.name} { todo.completed?<span>&#x2713;</span> : <span></span>} </li>)
-            })
-          }
-        </ul>
+        <TodoList todos={todos} />
+        <Form handleAdd={this.handleAdd} />
 
-        <form>
-        <h2>Add your tasks here</h2>
-          <input/>
-          <button>Add</button>
-        </form>
-        <button>Clear</button>
+        <button onClick={this.handleClear}>Clear</button>
       </div>
       
     )
