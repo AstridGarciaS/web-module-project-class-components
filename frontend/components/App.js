79f3689cfc3291a61bb99c781985489a6dd4d3c2
AstridGarciaS <119351821+AstridGarciaS@users.noreply.github.com>
2023-07-02 21:15:24 -0400

    styles

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -8,16 +8,7 @@ export default class App extends React.Component {
     super();
     this.state = {
       todos: [
-        {
-          task: 'Organize Garage',
-          id: 1528817077286, 
-          completed: false
-        },
-        {
-          task: 'Bake Cookies',
-          id: 1528817084358,
-          completed: false
-        }
+
       ]
     }
     
@@ -70,7 +61,7 @@ export default class App extends React.Component {
     const { todos } = this.state;
     return (
       <div>
-        <h2>Welcome to my Todo App!</h2>
+        <h2>Welcome to my To-do App!</h2>
         <TodoList handleToggle={this.handleToggle} todos={todos} />
         <Form handleAdd={this.handleAdd} />
 
--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -23,7 +23,11 @@ export default class Form extends React.Component {
 
     return (
       <form>
-        <h3>Add your tasks here</h3>
+        <h3>Instructions:</h3>
+            <p>Enter a task in the input field. For example, "Finish homework".</p>
+            <p>Click the "Add" button to add the task to the list of todos.</p>
+            <p>To clear completed tasks from the list, Click over the task and click the "Clear" button.</p>
+            <h3>Add your tasks here</h3>
         <input value={input} onChange={this.handleChange} />
         <button onClick={this.handleSubmit}>Add</button>
       </form>
